bb1474d225d19e7b2760ab86e21e77319f647f92
javed20sk00ali-pixel <javed20sk00ali@gmail.com>
2025-08-07 02:45:05 +0530

    frontend update

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Option Strategy Builder (Frontend Setup)</h1>
+      <p>This is a fresh React app ready to connect to your backend.</p>
+    </div>
+  );
+}
+
+export default App;
